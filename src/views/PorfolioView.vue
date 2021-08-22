94393b224ff9d6e66ece889b9a6a9ea83e2a6773
ColinCoolGitHub <colingoulethardy@gmail.com>
2021-08-22 16:54:38 -0400

    test chart

--- src/views/PorfolioView.vue
+++ src/views/PorfolioView.vue
@@ -1,13 +1,20 @@
 <template>
   <div>
-      <MouvementChart></MouvementChart>
+      Ici probablement pour suivre ton capital 
+      <line-chart></line-chart>
+      Ici pour regarder la diversification de ton porfolio
+      <donut-chart></donut-chart>
+      Ici pour les pourcentages de gain par stock
+      <bar-chart></bar-chart>
   </div>
 </template>
 
 <script>
-import MouvementChart from '../components/charts/MouvementChart.vue'
+import BarChart from '../components/charts/BarChart.vue'
+import DonutChart from '../components/charts/DonutChart.vue'
+import LineChart from '../components/charts/LineChart.vue'
 export default {
-  components: { MouvementChart },
+  components: { LineChart, DonutChart, BarChart },
 
 }
 </script>
